f4fcd8f0d04394380d64f9eb80acdb314d567b54
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-02 20:27:19 +0900

    [WIP] possible to output color

--- src/trot.nim
+++ src/trot.nim
@@ -1,2 +1,26 @@
-# Hello Nim!
-echo "Hello, World!"
+import os
+import osproc
+import strutils
+import sequtils
+import terminal
+from colors as cs import parseColor
+
+enableTrueColors()
+
+const
+  git_log = "git log --oneline --branches --reverse --since=\"1year\" --date=iso --pretty=format:\"%ad\""
+  colors = @["#FFFFFF", "#C6E48B", "#7BC96F", "#239A3B", "#196127"]
+  space = "  "
+
+for i in colors:
+  setBackgroundColor(stdout, parseColor(i))
+  stdout.write(space)
+  resetAttributes(stdout)
+  echo "reset"
+
+proc get_log(): string =
+  let (outp, errC) = execCmdEx(git_log)
+  return outp
+
+if isMainModule:
+  echo get_log()
