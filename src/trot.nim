09bf48d43fbacc81e4907c5f07a42ba561f291c3
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-04 13:50:28 +0900

    [WIP] add proc

--- src/trot.nim
+++ src/trot.nim
@@ -5,7 +5,9 @@ from colors as cs import parseColor
 enableTrueColors()
 
 const
+  Version = "0.1.0"
   colors = @["#FFFFFF", "#C6E48B", "#7BC96F", "#239A3B", "#196127"]
+  days = @["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
   space = "  "
 
 #[
@@ -15,6 +17,10 @@ for i in colors:
   resetAttributes(stdout)
   echo "reset"
 ]#
+
+proc print_glass(): void =
+  for i in days:
+    echo i
 
 proc main(): void =
   echo "Hello World"
