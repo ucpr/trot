aab5919ad0f061027d365ad15b3cfd30e92ea60a
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-05 23:09:09 +0900

    [WIP] xmlを正規表現で必要な情報を取るようにした

--- src/user.nim
+++ src/user.nim
@@ -1,9 +1,17 @@
 import ospaths
 import httpclient
+import strutils, sequtils
+import re
 
-let http_proxy = getEnv("http_proxy")
 let
-  client = newHttpClient(proxy=newProxy(http_proxy))
-#  client = newHttpClient()
+  http_proxy = getEnv("http_proxy")
+  regex = re""".*<rect class="day" .*>.*"""
+
+let
+#  client = newHttpClient(proxy=newProxy(http_proxy))
+  client = newHttpClient()
   body = client.getContent("https://github.com/users/nve3pd/contributions")
-echo body
+
+for i in body.strip().split("\n"):
+  if i.match(regex):
+    echo i.strip()
