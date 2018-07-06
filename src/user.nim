b340c61e988526148b3ccdf09bea092b470a475a
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-06 20:43:00 +0900

    [WIP] 正規表現じゃなくてPEGでやることにした

--- src/user.nim
+++ src/user.nim
@@ -1,15 +1,16 @@
 import ospaths
 import httpclient
 import strutils, sequtils
-import re
+import pegs
 
 let
   http_proxy = getEnv("http_proxy")
-  regex = re""".*<rect class="day" .*>.*"""
+  regex = peg""" """
+  reDate = peg" "
 
 let
-#  client = newHttpClient(proxy=newProxy(http_proxy))
-  client = newHttpClient()
+  client = newHttpClient(proxy=newProxy(http_proxy))
+#  client = newHttpClient()
   body = client.getContent("https://github.com/users/nve3pd/contributions")
 
 for i in body.strip().split("\n"):
