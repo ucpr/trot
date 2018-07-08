9f934d7354c476350c5750b095ffee905c3949bc
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-09 01:08:34 +0900

    [WIP] 色の情報と日付を取ってこれるようにした

--- src/user.nim
+++ src/user.nim
@@ -1,18 +1,23 @@
 import ospaths
 import httpclient
 import strutils, sequtils
-import pegs
+import nre, options
+
+import types
 
 let
   http_proxy = getEnv("http_proxy")
-  regex = peg""" """
-  reDate = peg" "
+  regex = re".*<rect.*>.*"
+  reColor = re"fill=\S#\w{6}\S"
+  reDate = re"data-date=\S\d{4}-\d{1,2}-\d{1,2}\S"
 
 let
-  client = newHttpClient(proxy=newProxy(http_proxy))
-#  client = newHttpClient()
+#  client = newHttpClient(proxy=newProxy(http_proxy))
+  client = newHttpClient()
   body = client.getContent("https://github.com/users/nve3pd/contributions")
 
 for i in body.strip().split("\n"):
-  if i.match(regex):
-    echo i.strip()
+  let r = i.match(regex)
+  if not r.isnone:
+    echo $($r.get()).find(reColor).get()
+    echo $($r.get()).find(reDate).get()
