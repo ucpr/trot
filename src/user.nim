4601198a84004c9b312a24a9c6f01c2b95c52af5
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-09 10:34:54 +0900

    [update] userを指定できるようにした

--- src/user.nim
+++ src/user.nim
@@ -10,12 +10,13 @@ let
   regex = re".*<rect.*>.*"
   reColor = re"#\w{6}"
   reDate = re"\d{4}-\d{1,2}-\d{1,2}"
+  URI = "https://github.com/users/$1/contributions"
 
   http_proxy = if existsEnv("http_proxy"): getEnv("http_proxy") else: nil
   client = newHttpClient(proxy = if http_proxy.isNil: nil else: newProxy(http_proxy))
-  body = client.getContent("https://github.com/users/nve3pd/contributions")
 
-proc userContributions*(): seq[colorData] =
+proc userContributions*(user: string): seq[colorData] =
+  let body = client.getContent(URI % user)
   result = @[]
   for i in body.strip().split("\n"):
     let r = i.match(regex)
@@ -28,5 +29,5 @@ proc userContributions*(): seq[colorData] =
       result.add(tmp)
 
 if isMainModule:
-  for i in userContributions():
+  for i in userContributions("nve3pd"):
     echo i
