6f26589ed84a8625e6fd13876be6ecb4bf0a681e
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-16 02:09:35 +0900

    [add] プロシージャを追加した

--- src/trotpkg/repo.nim
+++ src/trotpkg/repo.nim
@@ -1,6 +1,9 @@
 import os, osproc
 import strutils, sequtils
 
+import types
+export types.colorData
+
 const
   git_log = "git log --oneline --branches --reverse --since=\"1year\" --date=iso --pretty=format:\"%ad\""
  
@@ -10,6 +13,15 @@ proc get_log(): seq[string] =
     echo outp
     quit(1)
   return outp.strip().split("\n")
+
+proc sumLogs(): seq[string] =
+  return
+
+proc getAverage(): int =
+  return 1
+
+proc repoContributions*(): seq[colorData] =
+  result = @[]
 
 if isMainModule:
   echo get_log()
