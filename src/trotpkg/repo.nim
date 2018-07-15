1463f3c3e09d3f82feaa24dd98d46ef9732e5dd1
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-16 02:39:40 +0900

    [WIP] logの結果をまとめるプロシージャを作った

--- src/trotpkg/repo.nim
+++ src/trotpkg/repo.nim
@@ -1,12 +1,13 @@
 import os, osproc
 import strutils, sequtils
+import tables
 
 import types
 export types.colorData
 
 const
   git_log = "git log --oneline --branches --reverse --since=\"1year\" --date=iso --pretty=format:\"%ad\""
- 
+
 proc getLogs(): seq[string] =
   let (outp, errC) = execCmdEx(git_log)
   if errC != 0:
@@ -14,8 +15,10 @@ proc getLogs(): seq[string] =
     quit(1)
   return outp.strip().split("\n")
 
-proc sumLogs(): seq[string] =
-  return
+proc sumLogs(): CountTable[string] =
+  result = initCountTable[string]()
+  for log in getLogs():
+    result.inc(log.split()[0])
 
 proc getAverage(): int =
   return 1
@@ -24,4 +27,4 @@ proc repoContributions*(): seq[colorData] =
   result = @[]
 
 if isMainModule:
-  echo getLogs()
+  echo sumLogs()
