afe9d572d0a2d405cf5fd725f5419a4d61ca18aa
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-16 23:39:29 +0900

    [WIP] コマンドライン引数をparseしてhelp, versionを表示できるようにした

--- src/trot.nim
+++ src/trot.nim
@@ -1,8 +1,10 @@
 import os, osproc, terminal, times
 import strutils, sequtils
+import parseopt
+import strformat
 from colors as cs import parseColor
 
-import trotpkg/user
+import trotpkg/[user, repo]
 
 enableTrueColors()
 
@@ -10,6 +12,19 @@ const
   Version = "0.1.0"
   days = @["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   space = "  "
+  
+proc writeHelp(): void =
+  echo """
+  trot is a CLI Application that expresses grass of GitHub at terminal.
+  Usage: trot [OPTION] [username]
+         trot -h | --help
+         trot -v | --version
+  Option:
+    -h --help      show this screen.
+    -v --version   show version."""
+
+proc writeVersion(): void =
+  echo fmt"trot version {Version}"
 
 proc outputColor(color: string): void =
   setBackgroundColor(stdout, parseColor(color))
@@ -27,10 +42,26 @@ proc print_glass(glassData: seq[colorData]): void =
 
 proc main(): void =
   if paramCount() == 0:
+    print_glass(repoContributions())
     quit(0)
 
-  let user = commandLineParams()[0]
-  print_glass(userContributions(user))
+  let params = commandLineParams()
+  var p = initOptParser(params)
+  for kind, key, val in p.getopt():
+    case kind
+    of cmdArgument:
+      let glassData = userContributions(key)
+      if glassData == nil:
+        echo fmt"cannot find {val}"
+        quit(1)
+      print_glass(glassData)
+    of cmdLongOption, cmdShortOption:
+      case key
+      of "help", "h": writeHelp()
+      of "version", "v": writeVersion()
+    of cmdEnd: assert(false)  # cannot happen
+
+#  print_glass(repoContributions())
 
 if isMainModule:
   main()
